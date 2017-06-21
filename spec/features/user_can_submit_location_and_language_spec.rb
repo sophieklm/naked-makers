3efc7eefb8de0b45c1f978e93c99aeb898fd2e29
sophieklm <sklm@live.co.uk>
2017-06-21 15:00:27 +0100

    adds form fields

--- spec/features/user_can_submit_location_and_language_spec.rb
+++ spec/features/user_can_submit_location_and_language_spec.rb
@@ -1,7 +1,15 @@
 feature 'user can submit location & langauge' do
   scenario 'user finds the form' do
     visit '/'
-    click_on 'Submit data'
+    click_on 'Submit Data'
     expect(page).to have_content('Your data here:')
+  end
+  scenario 'user can see a location field' do
+    visit 'responses/new'
+    expect(page).to have_content('Location:')
+  end
+  scenario 'user can see a languages field' do
+    visit 'responses/new'
+    expect(page).to have_content('Languages:')
   end
 end
