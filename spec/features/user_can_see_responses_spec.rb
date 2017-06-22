15e845a24702acb66ad397c123984a964faa69cd
anaalta <suciuanamaria@yahoo.com>
2017-06-22 13:10:27 +0200

    added flash notice - data submitted

--- spec/features/user_can_see_responses_spec.rb
+++ spec/features/user_can_see_responses_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 
 RSpec.feature "Responses page",  type: :feature do
 
-  scenario "Can see responses page", type: :feature do skip
+  scenario "Can see responses page", type: :feature do
     visit('/')
     click_on('Submit Data')
     fill_in "response_location", :with => 'Birmingham'
     check('response_languages_', match: :first)
+    click_on 'Save Response'
     expect(page).to have_content('Data submitted')
   end
-
-
 end
