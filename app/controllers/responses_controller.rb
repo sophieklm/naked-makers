ca481724cb9c98bd44f415920a3820c77aac0fd6
sophieklm <sklm@live.co.uk>
2017-06-24 19:54:35 +0100

    updates to fix error on nil languages

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -7,14 +7,25 @@ class ResponsesController < ApplicationController
   def create
     @response ||= Response.new
     @response.save
-    response_params[:languages].each do |language|
-      create_language_join_response_path(language, @response.id)
+    if response_params[:languages]
+      create_languages
     end
     if response_params[:city] != ""
-      redirect_to response_locations_path(response_id: @response.id, city: response_params[:city])
+      create_response_location(@response.id, response_params[:city])
     else
       redirect_to root_url
       flash[:notice] = "Data submitted"
+    end
+  end
+
+  private
+  def response_params
+    params.require(:response).permit(:city, languages: [])
+  end
+
+  def create_languages
+    response_params[:languages].each do |language|
+      create_language_join_response_path(language, @response.id)
     end
   end
 
@@ -23,17 +34,8 @@ class ResponsesController < ApplicationController
     @response_language.save
   end
 
-  private
-  def response_params
-    params.require(:response).permit(:city, languages: [])
+  def create_response_location(response_id, city)
+    redirect_to response_locations_path(response_id: response_id, city: city)
   end
 
-  def response_location
-    if response_params[:city] != ""
-      redirect_to response_locations_path(response_id: @response.id, city: response_params[:city])
-    else
-      redirect_to root_url
-      flash[:notice] = "Data submitted"
-    end
-  end
 end
