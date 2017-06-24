19c94212e8397e8ee95b86204ba76c795fa55dd2
sophieklm <sklm@live.co.uk>
2017-06-24 16:59:41 +0100

    empty location is not saved

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -7,7 +7,12 @@ class ResponsesController < ApplicationController
   def create
     @response = Response.new(languages: response_params[:languages])
     @response.save
-    redirect_to response_locations_path(response_id: @response.id, city: response_params[:city])
+    if response_params[:city] != ""
+      redirect_to response_locations_path(response_id: @response.id, city: response_params[:city])
+    else
+      redirect_to root_url
+      flash[:notice] = "Data submitted"
+    end
   end
 
   private
