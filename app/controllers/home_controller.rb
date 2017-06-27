38ba13fcd1f49ac9d73bd6e3eea1d18db2e4829d
sophieklm <sklm@live.co.uk>
2017-06-27 20:26:41 +0100

    updates home controller methods

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,14 +2,6 @@ class HomeController < ApplicationController
   def index
   end
 
-  def language_data
-    respond_to do |format|
-      format.json {
-        render :json => create_language_hash(ResponseLanguage, :language_id)
-      }
-    end
-  end
-
   def location_language_data
     respond_to do |format|
       format.json {
@@ -24,26 +16,20 @@ class HomeController < ApplicationController
     data = []
     @locations = Location.all
     group_count(model, group_by).each do |key, value|
-      languages = []
-      @languages = Language.all
-      group_count(LocationLanguage.where(location_id: key), :language_id).each do |key, value|
-        key = @languages.find_by(id: key).name
-        languages << { name: key, count: value }
-      end
-      key = @locations.find_by(id: key).city
-      data << { name: key, children: languages }
+      name = @locations.find_by(id: key).city
+      data << { name: name, children: create_language_hash(key) }
     end
     return data
   end
 
-  def create_language_hash(model, group_by)
-    data = []
+  def create_language_hash(location_id)
+    languages = []
     @languages = Language.all
-    group_count(model, group_by).each do |key, value|
+    group_count(LocationLanguage.where(location_id: location_id), :language_id).each do |key, value|
       key = @languages.find_by(id: key).name
-      data << { name: key, count: value }
+      languages << { name: key, count: value }
     end
-    return data
+    return languages
   end
 
   def group_count(model, group_by)
