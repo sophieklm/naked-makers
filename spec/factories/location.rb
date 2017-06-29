c1a23c14b82892fbc04eb610b53c45af802e3839
sophieklm <sklm@live.co.uk>
2017-06-29 17:12:32 +0100

    updates controllers and specs

--- spec/factories/location.rb
+++ spec/factories/location.rb
@@ -4,6 +4,10 @@ FactoryGirl.define do
     response = FactoryGirl.create(:response)
     location.response_id = response.id
   end
-    city "London"
+  city "London"
+  after(:create) do |location|
+    responselocation = FactoryGirl.create(:responselocation, response_id: location.response_id, location_id: location.id)
+    locationlanguage = FactoryGirl.create(:locationlanguage, location_id: location.id)
+  end
   end
 end
---
+++ spec/factories/locationlanguage.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :locationlanguage, class: LocationLanguage do
+    location_id 1
+    language_id 1
+  end
+end
